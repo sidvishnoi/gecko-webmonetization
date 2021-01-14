dd8fa0ad0058ec7455d13efe99cd87b9e0d3eb70
Sid Vishnoi <sidvishnoi8@gmail.com>
2021-01-14 20:51:15 +0530

    (WM): nit: add some jsdoc comments

--- browser/components/webmonetization/NavigatorMonetization.js
+++ browser/components/webmonetization/NavigatorMonetization.js
@@ -2,8 +2,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 "use strict";
+
 var EXPORTED_SYMBOLS = ["Monetization"];
 
+/**
+ * `window.navigator.monetization` interface.
+ * See {@link dom/webidl/Monetization.webidl} for interface definition.
+ */
 class Monetization {
   init() {}
 
